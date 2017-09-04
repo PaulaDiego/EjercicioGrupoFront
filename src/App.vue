6b5b2743cecef975eb9991f4343eff6a83babf9a
paulacurso <paulacursocic@gmail.com>
2017-09-04 16:45:39 +0200

    Avances en Plantillas

--- src/App.vue
+++ src/App.vue
@@ -1,21 +1,45 @@
 <template>
   <div id="app" class="row center-block col-md-offset-2 col-md-10">
-    <h1 id="plantilla" class="col-spaced col-md-6 btn btn-secondary btn btn-md">Plantillas</h1>
-    <h1 id="documentos" class="col-spaced col-md-6 btn btn-secondary btn btn-md">Tipos de Documento</h1>
+    <h1 id="plantilla" v-on:click="showMaster" class="col-spaced col-md-6 btn btn-secondary btn btn-md">Plantillas</h1>
+    <h1 id="documento" v-on:click="showMaster" class="col-spaced col-md-6 btn btn-secondary btn btn-md">Tipos de Documento</h1>
+    <div id="master"></div>
   </div>
+  
 </template>
 <script>
 import Vue from 'vue'
+import PlantillaMaster from './PlantillaMaster.vue'
 export default {
   name: 'app',
   data(){
     return {
-
+      plantillaSelected :false,
+      documentoSelected:false
     }
     
   },
   methods: {
-    
+    showMaster:function(event){
+      let id = event.target.id
+      switch (id){
+        case "plantilla":
+            this.plantillaSelected=true
+            new Vue({
+              el: '#master',
+              render: h => h(PlantillaMaster)
+            })
+            this.documentoSelected=false
+          break
+        case "documentos":
+            this.documentoSelected=true
+            /*new Vue({
+              el: '#master',
+              render: h => h(DocumentoMaster)
+            })*/
+            this.plantillaSelected=false
+          break
+      }
+    }
   }
 }
 </script>
---
+++ src/PlantillaMaster.vue
@@ -0,0 +1,63 @@
+<template>
+<div id="master" class="center-block">
+	<ul v-if = "plantillas"  class="list-group">
+	<p>Seleccione una plantilla para editarla o cree una nueva plantilla</p>
+	<p><input type="button" name="crear" value="Nueva Plantilla" v-on:click = "showDetail"/></p>
+	<a href="#" class="list-group-item row col-md-4" v-for= "entrada in entradas" v-on:click= "showDetail" id="plantilla.Id" v-bind:id = "plantilla.Id">Plantilla Tipo: {{plantilla.Tipo}}</a>
+	</ul> 
+	<div v-else>
+	<p>No hay plantilas disponibles cree una nueva plantilla</p>
+	<p><input type="button" name="crear" value="Nueva Plantilla" v-on:click = "showDetail"/></p>
+	</div>
+	<div id="detail"></div>
+</div>
+</template>
+
+<script>
+import axios from 'axios'
+import Vue from 'vue'
+import {EventBus} from './EventBus.js'
+export default{
+	name: 'master',
+	data () {
+		return {
+			plantillas: null
+		}
+	},
+	created(){
+		this.getAll()
+		EventBus.$on('cambiosPlantilla',
+			this.getAll
+		)
+		
+	},
+	methods: {
+		getAll: function(){
+			axios.get('http://10.60.23.11:50514/api/plantillas')
+			.then (result => {
+				this.plantillas = result.data;
+			})
+		},
+		showDetail: function(event){
+			let plantilla = this.devuelvePlantilla(event.target.id)
+			new Vue({
+				el: '#detalle',
+				data:{
+					plantilla:plantilla
+				},
+				render: h => h(DetalleEntrada)
+			})
+		},
+		devuelvePlantilla: function(id){
+			var resultado
+			this.plantillas.forEach(function(plantilla){
+				if(plantilla.Id == id){
+					resultado = plantilla
+				}		
+
+			})
+			return resultado
+		}
+	}
+}
+</script>
