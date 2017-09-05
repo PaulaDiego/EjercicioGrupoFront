dd2854167db8cfe8d6b827378e40a09fe6088d39
paulacurso <paulacursocic@gmail.com>
2017-09-05 18:27:23 +0200

    Elimino node_modules (PD:yo no he sido)

--- src/App.vue
+++ src/App.vue
@@ -29,7 +29,7 @@ export default {
               el: '#master',
               render: h => h(PlantillaMaster)
             })
-            this.tipodocSelected=false
+            this.tipoDocSelected=false
           break
         case "tipoDoc":
             this.tipoDocSelected=true
